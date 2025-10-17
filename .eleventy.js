3e35adcf5d5016e20b170a7e018991d83417988e
John Alferez <johnalferez100@gmail.com>
2025-10-17 00:01:43 -0500

    Initial commit!

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("year", () => new Date().getFullYear());
+
+  eleventyConfig.addPassthroughCopy("images");
+
+
+  eleventyConfig.addPassthroughCopy("admin");
+
+ 
+  eleventyConfig.addCollection("bread", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/bread/*.md");
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
---
+++ content/bread/sourdough-bread.md
@@ -0,0 +1,6 @@
+---
+title: Sourdough Bread
+price: "10.00"
+image: /images/uploads/images.jpg
+date: 2025-10-14T21:38:00.000-05:00
+---
